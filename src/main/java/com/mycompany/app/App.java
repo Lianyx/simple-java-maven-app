18404288a3632aa03b9efee09665ba57a4d8c02b
71895167 <1023596812@qq.com>
2018-03-13 12:00:24 +0800

    hopefully

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!" + "no ssh key";
+    private final String message = "Hello World!";
 
     public App() {}
 
